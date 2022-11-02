99035355ef549a8b160b213828b6d08f67e5af9d
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2022-11-03 02:36:03 +0530

    Create 433. Minimum Genetic Mutation.java

---
+++ November Challenge/433. Minimum Genetic Mutation.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int minMutation(String start, String end, String[] bank) {
+
+        Queue<String> queue = new LinkedList<>();
+        queue.add(start);
+        boolean [] vis = new boolean[bank.length];
+     
+
+        int minMutation=0;
+        while(queue.size()!=0)
+        {
+            int size = queue.size();
+            
+            for(int i=0;i<size ;i++){
+                
+                String curr = queue.poll();
+                if(curr.equals(end)) return minMutation;
+
+                for(int j=0 ;j<bank.length ;j++){
+                    if(oneCharDiff(curr , bank[j]) && !vis[j]){
+                        vis[j] = true;
+                        queue.add(bank[j]);
+                    }
+                }
+            }
+            
+            minMutation++;
+
+        }
+
+        return -1;
+
+    }
+
+     public boolean oneCharDiff(String a , String b){
+
+        int differences=0;
+
+        for(int i=0;i<8 ;i++){
+            if(a.charAt(i)!=b.charAt(i)){
+                if(differences==1) return false;
+                differences++;
+            }
+        }
+
+        return differences==1 ? true : false;
+    }
+}
